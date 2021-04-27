7ee7cd41c370f2dea2af3f47d3fe4b14284c8d72
bogiszi <bogardiszilvia.pm@gmail.com>
2021-04-27 08:59:56 +0200

    Hobbyproject1 with colors

--- src/com/jetbrains/ideatest2/Table.java
+++ src/com/jetbrains/ideatest2/Table.java
@@ -4,15 +4,24 @@ import java.util.List;
 
 public class Table {
 
-    final String TEXT_RESET = "\u001B[0m";
-    final String TEXT_BLACK = "\u001B[30m";
-    final String TEXT_RED = "\u001B[31m";
-    final String TEXT_GREEN = "\u001B[32m";
-    final String TEXT_YELLOW = "\u001B[33m";
-    final String TEXT_BLUE = "\u001B[34m";
-    final String TEXT_PURPLE = "\u001B[35m";
-    final String TEXT_CYAN = "\u001B[36m";
-    final String TEXT_WHITE = "\u001B[37m";
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String TEXT_BLACK = "\u001B[30m";
+    public static final String TEXT_RED = "\u001B[31m";
+    public static final String TEXT_GREEN = "\u001B[32m";
+    public static final String TEXT_YELLOW = "\u001B[33m";
+    public static final String TEXT_BLUE = "\u001B[34m";
+    public static final String TEXT_PURPLE = "\u001B[35m";
+    public static final String TEXT_CYAN = "\u001B[36m";
+    public static final String TEXT_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     protected int rows;
     protected int columns;
@@ -70,7 +79,7 @@ public class Table {
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                System.out.print(TEXT_BLUE + board[i][j] + TEXT_RESET);
+                System.out.print(TEXT_BLACK + ANSI_BLUE_BACKGROUND + board[i][j] + TEXT_RESET);
             }
             System.out.println();
 
