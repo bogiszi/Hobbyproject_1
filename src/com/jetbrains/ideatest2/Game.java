392a1bc599cfd9739181f9ce1c43f663047d70c5
bogiszi <bogardiszilvia.pm@gmail.com>
2021-05-01 05:23:48 +0200

    Hobbyproject1 with JAVA FX

--- src/com/jetbrains/ideatest2/Game.java
+++ src/com/jetbrains/ideatest2/Game.java
@@ -7,6 +7,9 @@ public class Game {
 
 
     public String[] getQuestionAndAnswer(QuestionsAndAnswers qAndA, int difficultyInput) {
+        if (qAndA.getQuestionsAndAnswers().size() == 0) {
+            qAndA = new QuestionsAndAnswers();
+        }
         String[] questionAndAnswer = new String[5];
         for (Map.Entry<String, Integer> integerStringEntry : qAndA.getQuestionsAndAnswers().entrySet()) {
             if (integerStringEntry.getValue().equals(difficultyInput)) {
